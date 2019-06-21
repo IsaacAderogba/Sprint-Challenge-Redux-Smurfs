c6a5980d5f222709b2724eabdc8ee0c50954a8a2
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-21 09:11:45 +0100

    Add Form markup to SmurfsForm component

--- smurfs/src/components/SmurfsForm/SmurfsForm.js
+++ smurfs/src/components/SmurfsForm/SmurfsForm.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const SmurfsForm = () => {
-    return(
-        <div>SmurfsForm</div>
-    )
-}
+  let nameRef = React.createRef();
+  let ageRef = React.createRef();
+  let heightRef = React.createRef();
+
+  const onSubmitForm = event => {
+    event.preventDefault();
+    const name = nameRef.current.value;
+    const age = ageRef.current.value;
+    const height = heightRef.current.value + "cm";
+    // call postSmurf
+  };
+
+  return (
+    <form>
+      <h3>Add Smurf</h3>
+      <div>
+        <label>Name</label>
+        <input type="text" ref={nameRef} />
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="text" ref={ageRef} />
+      </div>
+      <div>
+        <label>Height</label>
+        <input type="text" ref={heightRef} />
+      </div>
+      <button>Add Smurf</button>
+    </form>
+  );
+};
 
 export default SmurfsForm;
